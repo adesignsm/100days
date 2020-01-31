275e117f4f8c3d9eaf0e42598d0d73e6a9168fa8
freakolope <akashmulye97@gmail.com>
2020-01-31 02:08:08 -0500

    day3 and day 4

--- day2/scripts/main.js
+++ day2/scripts/main.js
@@ -1,19 +1,18 @@
 var hours = document.getElementById("hours");
 var minutes = document.getElementById("minutes");
 var seconds = document.getElementById("seconds");
+var h, m, s, today;
 
 function getTimeandDate() {
 
-	var today = new Date();
+	today = new Date();
 	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 	document.getElementById("date").textContent = [today.getDate(), months[today.getMonth()], today.getFullYear()].join(" / ");
-	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
+	m = today.getMinutes();
+	s = today.getSeconds();
 
-	hours.textContent = h - 12;
 	minutes.textContent = m;
 	seconds.textContent = s;
 
@@ -26,20 +25,58 @@ function getTimeandDate() {
 	document.getElementById("progress-bar-s").style.width = s + "%";
 
 	setTimeout(getTimeandDate, 1000);
+}
 
+today = new Date();
+h = today.getHours();
+hours.textContent = h - 12;
+
+var inc_btn = document.getElementById("inc-btn");
+var dec_btn = document.getElementById("dec-btn");
+var multiplier = 0;
+
+function changeTime(time, x) {
+
+	inc_btn.style.opacity = 1;
+	dec_btn.style.opacity = 1;
+
+	multiplier++;
+
+	today = new Date();
+	h = today.getHours();
+
+	inc_btn.onclick = function(event) {
+
+		console.log(multiplier);
+
+		x = x + multiplier;
+		time.textContent = x;
+
+		if (time.textContent >= 24 || time.textContent >= 12) {
+
+			if (time.textContent == 23) {
+
+				time.textContent = 11;
+			}
+
+			x = 0;
+			x.setHours()
+
+		} else {
+
+			time.textContent = x;
+		}
+
+		console.log(x);
+	}
+
+	console.log(time);
 }
 
 hours.onclick = function() {
 
 	console.log("hours");
-}
-
-minutes.onclick = function() {
-	console.log("minutes");
-}
-
-seconds.onclick = function() {
-	console.log("seconds");
+	changeTime(this, h);
 }
 
 getTimeandDate();
